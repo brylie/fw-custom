3532014dbceba9d4e139d717c583323de3df7394
Brylie Christopher Oxley <brylie@gnumedia.org>
2014-08-06 04:28:43 -0400

    Initial commit.

---
+++ image_field_caption.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * $image - contains the image html rendered by Drupal
+ * $caption - contains the image field caption string
+ */
+?>
+<figure>
+  <?php print $image; ?>
+  <figcaption class"image_field_caption">
+    <?php print $caption; ?>
+  </figcaption>
+</figure>
